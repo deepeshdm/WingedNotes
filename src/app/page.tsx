7c315ecc0dbd5c695bb30138265768fdbd701971
deepeshdm <deepeshmhatre133@gmail.com>
2023-03-16 11:55:10 +0530

    added responsive /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import { ButterFlyBackground } from "./components/animatedBackground.jsx"
 
 // Import 'Raleway' Google Font
 const ralewayFont = Raleway({ weight: '700', subsets: ['latin'] });
-const headStyles = `text-white font-bold text-7xl head-shadow ${ralewayFont.className}`;
 
 
 export default function Home() {
@@ -33,19 +32,22 @@ export default function Home() {
 
       <div id="main-content" className="absolute top-0 left-0 h-full w-full flex flex-col items-center justify-center select-none">
 
-        <div className="flex flex-col items-center justify-center px-10 pt-14 bg-gray-700 bg-opacity-70 rounded-md shadow-md">
+        <div className="flex flex-col items-center justify-center bg-gray-700 bg-opacity-70 rounded-md shadow-md
+          pt-8 pb-4 px-5      sm:px-10 sm:pt-14 ">
 
-          <div className="flex space-x-2 items-center">
+          <div className="flex space-x-2 items-center max-sm:flex-col max-sm:space-y-2">
             <Image src="/memo_emoji.png" alt="" width={90} height={90} className="mr-1" />
-            <h1 className={headStyles}>Winged<span className="text-sky-500">NOTES</span></h1>
+            <h1 className={`text-white font-bold text-4xl sm:text-5xl md:text-6xl head-shadow ${ralewayFont.className}`}>
+              Winged<span className="text-sky-500">NOTES</span>
+            </h1>
           </div>
 
-          <p className="font-light text-white mt-3 text-xl head-shadow">
+          <p className="font-light text-white mt-3 text-center text-base sm:text-xl head-shadow">
             "Keep all your notes in one place with a cloud-based Notepad"
           </p>
 
 
-          <div className="flex space-x-10 mt-14">
+          <div className="flex space-x-10 mt-14 max-sm:flex-col max-sm:space-x-0 max-sm:space-y-5">
             <Link href="/login" className="bg-gradient-to-r from-purple-400 to-pink-500 hover:from-pink-500 hover:to-purple-600 active:from-pink-600 active:to-purple-700 text-white text-lg font-semibold py-3 px-8 rounded-md shadow-md transition duration-300">
               LOGIN
             </Link>
@@ -54,9 +56,9 @@ export default function Home() {
             </Link>
           </div>
 
-         <a href="https://github.com/deepeshdm/WingedNotes" target="_blank" >
-         <p className="font-medium text-white opacity-70 text-xs head-shadow mt-10 mb-5"> Github@deepeshdm  </p>
-         </a>
+          <a href="https://github.com/deepeshdm/WingedNotes" target="_blank" >
+            <p className="font-medium text-white opacity-70 text-xs head-shadow mt-10 mb-5"> Github@deepeshdm  </p>
+          </a>
 
         </div>
       </div>
