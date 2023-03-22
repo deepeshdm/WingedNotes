5b794a6d6ffa95483bfba0a996950fae4532221d
deepeshdm <deepeshmhatre133@gmail.com>
2023-03-22 10:19:51 +0530

    updated edit_note

--- src/app/create_note/UpdateButton.tsx
+++ src/app/create_note/UpdateButton.tsx
@@ -3,52 +3,58 @@
 
 import { updatePost } from '@/db_utils';
 import { AlertColor } from '@mui/material';
-import {BsFillArrowUpSquareFill} from "react-icons/bs"
-import {useMediaQuery} from '@mui/material';
+import { BsFillArrowUpSquareFill } from "react-icons/bs"
+import { useMediaQuery } from '@mui/material';
 
 type ButtonProps = {
-    noteData: { title: string; body: string },
-    postId: string,
-    loggedInUser: string,
-    disableUpdateButton: boolean,
-    setDisableUpdateButton: React.Dispatch<React.SetStateAction<boolean>>,
-    setSnackbarState: (message: string, severity: AlertColor, open: boolean) => void
+  noteData: { title: string; body: string },
+  postId: string,
+  loggedInUser: string,
+  disableUpdateButton: boolean,
+  setDisableUpdateButton: React.Dispatch<React.SetStateAction<boolean>>,
+  setSnackbarState: (message: string, severity: AlertColor, open: boolean) => void
 }
 
 
 export const UpdateButton = ({ noteData, postId, loggedInUser, disableUpdateButton, setDisableUpdateButton, setSnackbarState }: ButtonProps) => {
 
-    const isSmallScreen = useMediaQuery('(max-width:450px)');
+  const isSmallScreen = useMediaQuery('(max-width:450px)');
 
-    // Update the state to Database when Update Button clicked
-    const OnClickHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        console.log("Updating the Post !")
-        updatePost(loggedInUser, postId, noteData)                        // Update Post
-        setSnackbarState("Post Updated Successfully !", "success", true);  // Show Snackbar
-        setDisableUpdateButton(true)                                          // Disable Button
-        console.log("Post updated successfully !")
+  // Update the state to Database when Update Button clicked
+  const OnClickHandler = () => {
+
+    if (disableUpdateButton) {
+      return
     }
 
-    if (isSmallScreen) {
-        return (
-          <div className="max-[500px]:px-4 max-[500px]:ml-4">
-            <BsFillArrowUpSquareFill
-              className="text-sky-500 hover:text-sky-600 active:text-sky-500 cursor-pointer w-9 h-8"
-            />
-          </div>
-        );
-      }
+    console.log("Updating the Post !")
+    updatePost(loggedInUser, postId, noteData)                        // Update Post
+    setSnackbarState("Post Updated Successfully !", "success", true);  // Show Snackbar
+    setDisableUpdateButton(true)                                          // Disable Button
+    console.log("Post updated successfully !")
+  }
 
+  if (isSmallScreen) {
     return (
-        <>
-            <button
-                onClick={OnClickHandler}
-                disabled={disableUpdateButton}
-                className=" bg-sky-500 text-white font-bold px-7 py-2 rounded-md hover:bg-sky-600 active:bg-sky-700 
-                disabled:opacity-50 disabled:cursor-not-allowed ml-8 max-[500px]:px-4 max-[500px]:ml-5">
-                UPDATE
-            </button>
+      <div className="max-[500px]:px-4 max-[500px]:ml-4">
+        <BsFillArrowUpSquareFill onClick={OnClickHandler}
+          className={`text-sky-500 hover:text-sky-600 active:text-sky-500 cursor-pointer w-9 h-8 
+              ${disableUpdateButton ? 'opacity-50 cursor-not-allowed' : ''} `}
+        />
+      </div>
+    );
+  }
 
-        </>
-    )
+  return (
+    <>
+      <button
+        onClick={OnClickHandler}
+        disabled={disableUpdateButton}
+        className=" bg-sky-500 text-white font-bold px-7 py-2 rounded-md hover:bg-sky-600 active:bg-sky-700 
+                disabled:opacity-50 disabled:cursor-not-allowed ml-8 max-[500px]:px-4 max-[500px]:ml-5">
+        UPDATE
+      </button>
+
+    </>
+  )
 }
